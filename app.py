e6f24987145759558381a396aee9d11089907a02
zyj <edenzyj88@gmail.com>
2024-05-30 20:30:26 +0800

    call LLaMA-3 when recieve message.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,8 +10,17 @@ def index():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message')
+    # print(type(user_message))
+    user_message_list = []
+    user_message_list.append(user_message)
+    # print(type(user_message_list))
+    
+    subprocess.run(["torchrun", "--nproc_per_node=1",
+                    "/home/pcs4090/LLaMA/llama3/call_llama.py",
+                    "--queries {}".format(user_message_list)])
+    
     bot_response = "This is a bot response to: " + user_message
     return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8100, debug=True)
