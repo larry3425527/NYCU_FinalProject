785ab8c4d28492f9773974ac2d8f4ffe5750dc91
zyj <edenzyj88@gmail.com>
2024-06-07 00:52:55 +0800

    use socket rather than os.popen

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+
 import os
 import time
+import socket
+import pickle
 
 app = Flask(__name__)
 
@@ -13,22 +16,20 @@ def chat():
     user_message = request.json.get('message')
     # print(type(user_message))
     
-    # read_user_message(user_message=user_message)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8101))
+    data = pickle.dumps(user_message)
+    client_socket.send(data)
     
-    response = os.popen("torchrun --nproc_per_node 1 \
-             /home/pcs4090/LLaMA/llama3/call_llama.py \
-             --ckpt_dir /home/pcs4090/LLaMA/llama3/Meta-Llama-3-8B \
-             --tokenizer_path /home/pcs4090/LLaMA/llama3/Meta-Llama-3-8B/tokenizer.model \
-             --query '{}'".format(user_message)).read()
-    
-    response = response.split("Answer =")[-1].split("===")[0]
+    response = client_socket.recv(4096)
+    result = pickle.loads(response)
+    # print(result)
     
     bot_response = "This is a bot response to: \n{}\n".format(user_message)
-    time.sleep(8)
-    bot_response = bot_response + "\n" + response
+    # time.sleep(8)
+    bot_response = bot_response + "\n" + result['generation']
     
     return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8100, debug=True)
-    
